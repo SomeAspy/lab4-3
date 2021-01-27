7c966db4abc96844f4258caabbfdc96012333774
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2021-01-27 15:32:42 -0500

    Update code.js

--- code.js
+++ code.js
@@ -2,13 +2,15 @@ let names = ["Eric", "Sally", "Michelle", "John", "Sam", "Caleb"];
 function writeHTML(id, text) {
     document.getElementById(id).innerHTML = text;
 }
-function wait(ms){ //https://stackoverflow.com/questions/16873323/javascript-sleep-wait-before-continuing
-    let start=new Date().getTime();
-    for (let i=0; i<1e7; i++){
-        if ((new Date().getTime()-start)>ms){
-            break;
+function inOrder(){
+    names.forEach(element => {
+        alert(`Welcome, ${element}`)
         }
-    }
+    )
 }
 
-names.forEach
+document.addEventListener("keyup", (e) => {
+
+}
+
+})
